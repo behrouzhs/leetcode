a55cca6628ffbb6922d54f0c439e673816428ad7
behrouzhs <behrouzhs@gmail.com>
2024-12-05 22:57:47 -0500

    String solution to palindrome number

--- easy/q_0009_palindrome_number/q_0009_palindrome_number.py
+++ easy/q_0009_palindrome_number/q_0009_palindrome_number.py
@@ -38,4 +38,9 @@ Follow up: Could you solve it without converting the integer to a string?
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        pass
+        x_str = str(x)
+        n = len(x_str)
+        for i in range(n // 2):
+            if x_str[i] != x_str[n - i - 1]:
+                return False
+        return True
