aff41623e0460a08ce23aa8e837a0cda7c370aaf
behrouzhs <behrouzhs@gmail.com>
2024-12-13 22:33:16 -0800

    Solution to Roman to integer problem

--- easy/q_0013_roman_to_integer/q_0013_roman_to_integer.py
+++ easy/q_0013_roman_to_integer/q_0013_roman_to_integer.py
@@ -50,10 +50,26 @@ Constraints:
 1 <= s.length <= 15
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
-
-
 """
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        dct_roman = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
+        prev = 0
+        total = 0
+        for ch in s:
+            curr = dct_roman[ch]
+            if curr > prev:
+                total += curr - 2 * prev
+            else:
+                total += curr
+            prev = curr
+        return total
