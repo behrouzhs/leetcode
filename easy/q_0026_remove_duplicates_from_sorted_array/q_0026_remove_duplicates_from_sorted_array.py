ebcd275bc93052fbffe810700c5c583bbdb9234c
behrouzhs <behrouzhs@gmail.com>
2024-12-23 21:34:26 -0500

    Compact solution to remove duplicates

--- easy/q_0026_remove_duplicates_from_sorted_array/q_0026_remove_duplicates_from_sorted_array.py
+++ easy/q_0026_remove_duplicates_from_sorted_array/q_0026_remove_duplicates_from_sorted_array.py
@@ -64,3 +64,13 @@ class Solution:
                 i += 1
                 j += 1
         return i + 1, nums
+
+
+class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1, nums
