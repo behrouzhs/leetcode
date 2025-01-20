a88126cced19b002ed96aac869e7cb59f125b085
behrouzhs <behrouzhs@gmail.com>
2025-01-19 20:34:55 -0500

    Search insert position problem

---
+++ easy/q_0035_search_insert_position/q_0035_search_insert_position.py
@@ -0,0 +1,41 @@
+# URL: https://leetcode.com/problems/search-insert-position
+# Title: 35. Search Insert Position
+# Difficulty: Easy
+# Topics: Array, Binary Search
+# Likes: 16914, Dislikes: 793
+# Submitted: 7.3M, Accepted: 3.5M (48.0%)
+
+
+question = """
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+ 
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+ 
+Constraints:
+
+1 <= nums.length <= 10^4
+-10^4 <= nums[i] <= 10^4
+nums contains distinct values sorted in ascending order.
+-10^4 <= target <= 10^4
+
+
+"""
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        pass
